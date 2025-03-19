9c8886af6b9c46314eeef8b41cb1f65c80e4f105
Mateusz Kadula <mateusz.kadula@icloud.com>
2025-03-19 19:17:15 +0100

    Detection loop is now a function

--- licenseplate/main.py
+++ licenseplate/main.py
@@ -13,57 +13,39 @@ from .camera.base import CameraInterface
 from .action.base import ActionInterface
 
 
-class DetectionLoop:
-    def __init__(
-        self,
-        finder: LicensePlateFinder,
-        extractor: TextExtractor,
-        validator: LicensePlateValidator,
-        camera_interface: CameraInterface,
-        action_interface: ActionInterface,
-        max_fps: int = 30,
-    ):
-        self.finder = finder
-        self.extractor = extractor
-        self.validator = validator
-        self.camera = camera_interface
-        self.action = action_interface
-        self.max_fps = max_fps
+def detection_loop(
+    finder: LicensePlateFinder,
+    extractor: TextExtractor,
+    validator: LicensePlateValidator,
+    camera: CameraInterface,
+    action: ActionInterface,
+    max_fps: int = 30,
+) -> NoReturn:
+    fps_sum = 0.0
+    fps_count = 0
 
-    def loop(self) -> NoReturn:
-        fps_sum = 0.0
-        fps_count = 0
-
-        while True:
-            start = datetime.now()
-            frame = self.camera.get_frame()
-            plates = detect_plates(frame, self.finder, self.extractor, self.validator)
-            if plates:
-                self.action.action_if_found(frame, plates)
-            lasted = (datetime.now() - start).total_seconds()
-            fps_count += 1
-            fps_sum += 1 / lasted
-            print(f"FPS now: {1 / lasted}, FPS average: {fps_sum / fps_count}")
-            if 1 / self.max_fps - lasted > 0:
-                sleep(1 / self.max_fps - lasted)
-
-    def __call__(self) -> NoReturn:
-        self.loop()
+    while True:
+        start = datetime.now()
+        frame = camera.get_frame()
+        plates = detect_plates(frame, finder, extractor, validator)
+        if plates:
+            action.action_if_found(frame, plates)
+        lasted = (datetime.now() - start).total_seconds()
+        fps_count += 1
+        fps_sum += 1 / lasted
+        print(f"FPS now: {1 / lasted}, FPS average: {fps_sum / fps_count}")
+        if 1 / max_fps - lasted > 0:
+            sleep(1 / max_fps - lasted)
 
 
 if __name__ == "__main__":
     from .camera.macos import MacOSCameraInterface
     from .action.localsave import LocalSaveInterface
 
-    loop = DetectionLoop(
-        LicensePlateFinder(
-            Path(__file__).parents[1] / "runs/detect/train/weights/best.pt"
-        ),
+    detection_loop(
+        LicensePlateFinder(Path(__file__).parents[1] / "runs/detect/train/weights/best.pt"),
         TextExtractor(),
         LicensePlateValidator(),
         MacOSCameraInterface(),
-        LocalSaveInterface(
-            Path(__file__).parents[1] / "detected", show_debug_boxes=True
-        ),
+        LocalSaveInterface(Path(__file__).parents[1] / "detected", show_debug_boxes=True),
     )
-    loop()
