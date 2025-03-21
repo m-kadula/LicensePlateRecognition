f386ff728fd32b6233c94af8f958a0ccd3ff1147
Mateusz Kadula <mateusz.kadula@icloud.com>
2025-03-21 17:50:32 +0100

    setup for installation with pip

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LicensePlateRecognition",
+    version="0.1.0",
+    description="A daemon for license plate recognition from a live camera feed",
+    author="Mateusz Kadula",
+    author_email="mateusz.kadula@icloud.com",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "licenseplated=licenseplate.main:main",
+        ],
+    },
+    install_requires=[
+        "pyyaml",
+        "numpy",
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "ultralytics",
+        "pydantic",
+        "opencv-python",
+        "easyocr",
+        # "picamera2",
+    ],
+)
