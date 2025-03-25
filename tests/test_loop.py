75d5c3e0d780b553856e2f8d8c6b5b6860dff1ea
Mateusz Kadula <mateusz.kadula@icloud.com>
2025-03-25 21:59:00 +0100

    tests

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -1,22 +1,19 @@
-import sys
 from pathlib import Path
 import shutil
-from datetime import datetime
+from time import sleep
 
 from numpy.typing import NDArray
 import cv2
 
-from licenseplate.action.localsave import LocalSaveInterface
+from licenseplate.action.localsave import LocalSave, LocalSaveManager
 from licenseplate.camera.base import CameraInterface
-from licenseplate.detection import TextExtractor, ExtractorResult, PlateDetectionModel
+from licenseplate.detection import PlateDetectionModel
 from licenseplate.preprocessor.base import IdentityPreprocessor
 from licenseplate.preprocessor.polish_plate import PolishLicensePlatePreprocessor
-from licenseplate.logger import get_standard_logger
-from licenseplate.loop import DetectionLoop
 
 engine_dir = Path(__file__).parents[1] / "runs/detect/train/weights/best.pt"
 results_path = Path(__file__).parents[0] / "results"
-image_dir = Path(__file__).parents[1] / "dataset/images/val"
+image_dir = Path(__file__).parents[1] / "dataset/images"
 
 
 class MockCameraInterface(CameraInterface):
@@ -24,43 +21,61 @@ class MockCameraInterface(CameraInterface):
         self.image_dir = image_dir
         assert self.image_dir.exists() and self.image_dir.is_dir()
         self.image_iterator = self.image_dir.iterdir()
+        self.first = next(self.image_iterator)
 
     def get_frame(self) -> NDArray:
-        image_dir = next(self.image_iterator)
+        try:
+            image_dir = next(self.image_iterator)
+        except StopIteration:
+            image_dir = self.first
         return cv2.imread(image_dir)
 
 
-class DebugTextFinder(TextExtractor):
-    def run(self, image: NDArray) -> list[ExtractorResult]:
-        out = super().run(image)
-        if not (results_path.parent / "processed").exists():
-            (results_path.parent / "processed").mkdir()
-        cv2.imwrite(
-            str(
-                results_path.parent / "processed" / f"{datetime.now().isoformat()}.jpg"
-            ),
-            image,
-        )
-        return out
-
-
 def test_loop():
-    model = PlateDetectionModel(
+    model1 = PlateDetectionModel(
         Path(__file__).parents[1] / "runs/detect/train/weights/best.pt",
         IdentityPreprocessor(),
         PolishLicensePlatePreprocessor(),
         required_confidence=0.0,
     )
-    loop = DetectionLoop(
-        model,
-        MockCameraInterface(image_dir),
-        LocalSaveInterface(results_path, show_debug_boxes=True),
-        logger=get_standard_logger('detection_loop', sys.stdout)
+    model2 = PlateDetectionModel(
+        Path(__file__).parents[1] / "runs/detect/train/weights/best.pt",
+        IdentityPreprocessor(),
+        PolishLicensePlatePreprocessor(),
+        required_confidence=0.5,
     )
-    try:
-        loop.run()
-    except StopIteration:
-        pass
+    model3 = PlateDetectionModel(
+        Path(__file__).parents[1] / "runs/detect/train/weights/best.pt",
+        IdentityPreprocessor(),
+        PolishLicensePlatePreprocessor(),
+        required_confidence=0.8,
+    )
+    action1 = LocalSave(
+        model1,
+        MockCameraInterface(image_dir / 'val'),
+        30,
+        True
+    )
+    action2 = LocalSave(
+        model2,
+        MockCameraInterface(image_dir / 'train'),
+        30,
+        True
+    )
+    action3 = LocalSave(
+        model3,
+        MockCameraInterface(image_dir / 'val'),
+        30,
+        False
+    )
+    manager = LocalSaveManager(results_path)
+    manager.register_camera('camera1', action1, {})
+    manager.register_camera('camera2', action2, {})
+    manager.register_camera('camera3', action3, {})
+    manager.finish_registration()
+    manager.start()
+    sleep(5)
+    manager.stop()
 
 
 if __name__ == "__main__":
